b194d327a381b46d15715ebea98b17ae3d390ce9
meowlet <ngoclink13@gmail.com>
2021-04-20 16:40:22 +0700

    refactor(shared): port logger and color palette to typescript

---
+++ src/shared/colors.ts
@@ -0,0 +1,8 @@
+export const Colors = {
+  Primary: 0x7986cb,
+  Blurple: 0x9fa8da,
+  Wavenet: 0xffe082,
+  Success: 0xa5d6a7,
+  Warning: 0xfff59d,
+  Error: 0xef9a9a,
+} as const;
--- src/shared/logger.js
+++
@@ -1,8 +0,0 @@
-const levels = { info: 'INFO', warn: 'WARN', error: 'ERROR' };
-
-function log(level, message) {
-  const ts = new Date().toISOString();
-  console.log(`[${ts}] ${levels[level] || 'LOG'} ${message}`);
-}
-
-module.exports = { log };

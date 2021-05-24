27b2d11393e5f2502f6a0cee67c6f815b74a1fb3
meowlet <ngoclink13@gmail.com>
2021-05-24 10:30:11 +0700

    feat(help): add help embed builder

--- src/features/misc/help-command.ts
+++ src/features/misc/help-command.ts
@@ -18,3 +18,33 @@ interface CommandSummary {
   readonly category: CommandCategory;
 }
 
+export interface HelpCommandDeps {
+  readonly localeResolver: LocaleResolver;
+  readonly getCommands: () => readonly Command[];
+}
+
+export class HelpCommand implements Command {
+  readonly data = new SlashCommandBuilder()
+    .setName(en.commands.help.name)
+    .setDescription(en.commands.help.description)
+    .setDescriptionLocalizations({ vi: vi.commands.help.description });
+
+  private readonly localeResolver: LocaleResolver;
+  private readonly getCommands: () => readonly Command[];
+
+  constructor(deps: HelpCommandDeps) {
+    this.localeResolver = deps.localeResolver;
+    this.getCommands = deps.getCommands;
+  }
+
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    const locale = await this.localeResolver.resolve(interaction);
+    const summaries = this.getCommands().map((cmd) =>
+      this.summarize(cmd, locale),
+    );
+    const grouped: Record<CommandCategory, CommandSummary[]> = {
+      voice: [],
+      tts: [],
+      settings: [],
+      misc: [],
+    };

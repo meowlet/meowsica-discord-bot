82994f2d40cefd6118b5fd5768894aa241a923d5
meowlet <ngoclink13@gmail.com>
2023-03-22 11:15:44 +0700

    feat(infra): harden redis reconnect and tls settings

--- src/infra/redis.ts
+++ src/infra/redis.ts
@@ -38,3 +38,59 @@ export class RedisClient {
     try {
       this.client.close();
     } catch (err) {
+      this.logger.warn("Error closing Redis", err);
+    }
+    this.client = null;
+    this.connected = false;
+  }
+
+  async getJson<T>(key: string): Promise<T | null> {
+    if (!this.client) return null;
+    try {
+      const raw = await this.client.get(key);
+      if (!raw) return null;
+      return JSON.parse(raw) as T;
+    } catch (err) {
+      this.logger.warn(`getJson failed for ${key}`, err);
+      return null;
+    }
+  }
+
+  async setJson<T>(key: string, value: T, ttlSeconds?: number): Promise<void> {
+    if (!this.client) return;
+    try {
+      const payload = JSON.stringify(value);
+      if (ttlSeconds && ttlSeconds > 0) {
+        await this.client.send("SET", [
+          key,
+          payload,
+          "EX",
+          String(Math.floor(ttlSeconds)),
+        ]);
+        return;
+      }
+      await this.client.set(key, payload);
+    } catch (err) {
+      this.logger.warn(`setJson failed for ${key}`, err);
+    }
+  }
+
+  async del(key: string): Promise<void> {
+    if (!this.client) return;
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      this.logger.warn(`del failed for ${key}`, err);
+    }
+  }
+
+  async ping(): Promise<boolean> {
+    if (!this.client) return false;
+    try {
+      const result = await this.client.send("PING", []);
+      return result === "PONG" || result === "+PONG";
+    } catch {
+      return false;
+    }
+  }
+}

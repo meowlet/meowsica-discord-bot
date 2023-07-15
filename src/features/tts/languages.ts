6f09cd27433a63a0c139f2919860875cb9ebfc9a
meowlet <ngoclink13@gmail.com>
2023-07-15 14:28:33 +0700

    feat(tts): finalize language catalog and voice option tables

--- src/features/tts/languages.ts
+++ src/features/tts/languages.ts
@@ -148,3 +148,232 @@ export const SUPPORTED_LANGUAGES: readonly SupportedLanguage[] = [
     flag: "🇮🇳",
   },
   {
+    name: "Hungarian",
+    nativeName: "Magyar",
+    code: "hu",
+    cloudCode: "hu-HU",
+    flag: "🇭🇺",
+  },
+  {
+    name: "Icelandic",
+    nativeName: "Íslenska",
+    code: "is",
+    cloudCode: "is-IS",
+    flag: "🇮🇸",
+  },
+  {
+    name: "Indonesian",
+    nativeName: "Bahasa Indonesia",
+    code: "id",
+    cloudCode: "id-ID",
+    flag: "🇮🇩",
+  },
+  {
+    name: "Italian",
+    nativeName: "Italiano",
+    code: "it",
+    cloudCode: "it-IT",
+    flag: "🇮🇹",
+  },
+  {
+    name: "Javanese",
+    nativeName: "Basa Jawa",
+    code: "jw",
+    cloudCode: "jv-ID",
+    flag: "🇮🇩",
+  },
+  {
+    name: "Khmer",
+    nativeName: "ភាសាខ្មែរ",
+    code: "km",
+    cloudCode: "km-KH",
+    flag: "🇰🇭",
+  },
+  {
+    name: "Latvian",
+    nativeName: "Latviešu",
+    code: "lv",
+    cloudCode: "lv-LV",
+    flag: "🇱🇻",
+  },
+  {
+    name: "Malayalam",
+    nativeName: "മലയാളം",
+    code: "ml",
+    cloudCode: "ml-IN",
+    flag: "🇮🇳",
+  },
+  {
+    name: "Marathi",
+    nativeName: "मराठी",
+    code: "mr",
+    cloudCode: "mr-IN",
+    flag: "🇮🇳",
+  },
+  {
+    name: "Nepali",
+    nativeName: "नेपाली",
+    code: "ne",
+    cloudCode: "ne-NP",
+    flag: "🇳🇵",
+  },
+  {
+    name: "Norwegian",
+    nativeName: "Norsk",
+    code: "no",
+    cloudCode: "nb-NO",
+    flag: "🇳🇴",
+  },
+  {
+    name: "Polish",
+    nativeName: "Polski",
+    code: "pl",
+    cloudCode: "pl-PL",
+    flag: "🇵🇱",
+  },
+  {
+    name: "Portuguese",
+    nativeName: "Português",
+    code: "pt",
+    cloudCode: "pt-BR",
+    flag: "🇧🇷",
+  },
+  {
+    name: "Romanian",
+    nativeName: "Română",
+    code: "ro",
+    cloudCode: "ro-RO",
+    flag: "🇷🇴",
+  },
+  {
+    name: "Russian",
+    nativeName: "Русский",
+    code: "ru",
+    cloudCode: "ru-RU",
+    flag: "🇷🇺",
+  },
+  {
+    name: "Serbian",
+    nativeName: "Српски",
+    code: "sr",
+    cloudCode: "sr-RS",
+    flag: "🇷🇸",
+  },
+  {
+    name: "Sinhala",
+    nativeName: "සිංහල",
+    code: "si",
+    cloudCode: "si-LK",
+    flag: "🇱🇰",
+  },
+  {
+    name: "Slovak",
+    nativeName: "Slovenčina",
+    code: "sk",
+    cloudCode: "sk-SK",
+    flag: "🇸🇰",
+  },
+  {
+    name: "Spanish",
+    nativeName: "Español",
+    code: "es",
+    cloudCode: "es-ES",
+    flag: "🇪🇸",
+  },
+  {
+    name: "Sundanese",
+    nativeName: "Basa Sunda",
+    code: "su",
+    cloudCode: "su-ID",
+    flag: "🇮🇩",
+  },
+  {
+    name: "Swahili",
+    nativeName: "Kiswahili",
+    code: "sw",
+    cloudCode: "sw-TZ",
+    flag: "🇹🇿",
+  },
+  {
+    name: "Swedish",
+    nativeName: "Svenska",
+    code: "sv",
+    cloudCode: "sv-SE",
+    flag: "🇸🇪",
+  },
+  {
+    name: "Tamil",
+    nativeName: "தமிழ்",
+    code: "ta",
+    cloudCode: "ta-IN",
+    flag: "🇮🇳",
+  },
+  {
+    name: "Telugu",
+    nativeName: "తెలుగు",
+    code: "te",
+    cloudCode: "te-IN",
+    flag: "🇮🇳",
+  },
+  {
+    name: "Thai",
+    nativeName: "ไทย",
+    code: "th",
+    cloudCode: "th-TH",
+    flag: "🇹🇭",
+  },
+  {
+    name: "Turkish",
+    nativeName: "Türkçe",
+    code: "tr",
+    cloudCode: "tr-TR",
+    flag: "🇹🇷",
+  },
+  {
+    name: "Ukrainian",
+    nativeName: "Українська",
+    code: "uk",
+    cloudCode: "uk-UA",
+    flag: "🇺🇦",
+  },
+] as const;
+
+export const DEFAULT_LANGUAGE = SUPPORTED_LANGUAGES[0]!;
+export const DEFAULT_LANGUAGE_CODE = DEFAULT_LANGUAGE.code;
+export const DEFAULT_LANGUAGE_CLOUD = DEFAULT_LANGUAGE.cloudCode;
+
+export function findLanguageByCode(
+  code: string,
+): SupportedLanguage | undefined {
+  const exactCode = SUPPORTED_LANGUAGES.find((lang) => lang.code === code);
+  if (exactCode) return exactCode;
+  const exactCloud = SUPPORTED_LANGUAGES.find(
+    (lang) => lang.cloudCode === code,
+  );
+  if (exactCloud) return exactCloud;
+  const shortCode = code.includes("-") ? code.split("-")[0] : code;
+  return SUPPORTED_LANGUAGES.find((lang) => {
+    const langShort = lang.code.includes("-")
+      ? lang.code.split("-")[0]
+      : lang.code;
+    return langShort === shortCode;
+  });
+}
+
+export function isSupportedLanguage(code: string): boolean {
+  return findLanguageByCode(code) !== undefined;
+}
+
+export function toBasicLanguageCode(code: string): string {
+  const lang = findLanguageByCode(code);
+  return lang?.code ?? code.split("-")[0] ?? code;
+}
+
+export function toCloudLanguageCode(code: string): string {
+  const lang = findLanguageByCode(code);
+  return lang?.cloudCode ?? code;
+}
+
+export function getLanguageFlag(code: string): string {
+  return findLanguageByCode(code)?.flag ?? "🌐";
+}
